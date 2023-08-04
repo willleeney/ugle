39698c5e604dbd1b13b8abe8e2cedadc12040131
willleeney <will.leeney@outlook.com>
2023-08-04 11:38:10 +0100

    update bgrl

--- ugle/models/bgrl.py
+++ ugle/models/bgrl.py
@@ -116,7 +116,7 @@ class bgrl_trainer(ugleTrainer):
         outwards = [i[0] for i in adj_label.keys()]
         inwards = [i[1] for i in adj_label.keys()]
 
-        adj = torch.from_numpy(np.array([outwards, inwards], dtype=np.int))
+        adj = torch.from_numpy(np.array([outwards, inwards], dtype=int))
 
         data = torch.FloatTensor(features)
 

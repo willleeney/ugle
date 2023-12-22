0b466ced1717387cda4c1fdb5773d97b90bdaafb
willleeney <will.leeney@outlook.com>
2023-12-22 17:12:53 +0000

    changed log interval + echo in bash script

--- ugle_hpo.sh
+++ ugle_hpo.sh
@@ -2,10 +2,10 @@
 
 for a in sublime bgrl vgaer daegc dmon grace dgi; do
     for d in citeseer texas cora dblp cornell wisc; do
-        lbatch -c 1 -g 1 -m 22 -a EMAT022967 -q gpu --conda-env UFR --cmd  python3 model_evaluations.py -ec=ugle/configs/experiments/unsupervised_limit/hpo_new.yaml -da=${d}_${a}
+        echo -c 1 -g 1 -m 22 -a EMAT022967 -q gpu --conda-env UFR --cmd  python3 model_evaluations.py -ec=ugle/configs/experiments/unsupervised_limit/hpo_new.yaml -da=${d}_${a}
     done
 done
 
-lbatch -c 1 -g 1 -m 30 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec=ugle/configs/experiments/unsupervised_limit/dmon_large_computers.yaml
-lbatch -c 1 -g 1 -m 30 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec=ugle/configs/experiments/unsupervised_limit/dmon_large_photos.yaml
+echo -c 1 -g 1 -m 30 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec=ugle/configs/experiments/unsupervised_limit/dmon_large_computers.yaml
+echo -c 1 -g 1 -m 30 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec=ugle/configs/experiments/unsupervised_limit/dmon_large_photos.yaml
 

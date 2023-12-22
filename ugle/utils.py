ea33dcbb61ff55c0373affc6da45686e5fd0fb8d
willleeney <will.leeney@outlook.com>
2023-12-22 17:49:18 +0000

    changed config setup to not error

--- ugle/utils.py
+++ ugle/utils.py
@@ -230,16 +230,7 @@ def create_experiment_tracker(exp_cfg: DictConfig) -> list:
     :experiment_tracker: list of results objects that store results from individual experiment
     """
     experiment_tracker = []
-    if exp_cfg.datasets != [] and exp_cfg.algorithms != []:
-        for dataset in exp_cfg.datasets:
-            for algorithm in exp_cfg.algorithms:
-                experiment_tracker.append(OmegaConf.create(
-                                {'dataset': dataset,
-                                'algorithm': algorithm,
-                                'seeds': exp_cfg.seeds,
-                                'results': {}
-                                }))
-    elif exp_cfg.dataset_algo_combinations != []:
+    if exp_cfg.dataset_algo_combinations != []:
         for dataset_algo in exp_cfg.dataset_algo_combinations:
             dataset, algorithm = dataset_algo.split('_', 1)
             experiment_tracker.append(OmegaConf.create(
@@ -248,7 +239,16 @@ def create_experiment_tracker(exp_cfg: DictConfig) -> list:
                                 'seeds': exp_cfg.seeds,
                                 'results': {}
                                 }))
-
+            
+    if exp_cfg.datasets and exp_cfg.algorithms:
+        for dataset in exp_cfg.datasets:
+            for algorithm in exp_cfg.algorithms:
+                experiment_tracker.append(OmegaConf.create(
+                                {'dataset': dataset,
+                                'algorithm': algorithm,
+                                'seeds': exp_cfg.seeds,
+                                'results': {}
+                                }))
 
     return experiment_tracker
 

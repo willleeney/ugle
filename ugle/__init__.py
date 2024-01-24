5946230d01fc4bd7e6bdb0e939574a574b484d2d
willleeney <will.leeney@outlook.com>
2024-01-24 16:31:55 +0000

    correct version number

--- ugle/__init__.py
+++ ugle/__init__.py
@@ -3,7 +3,7 @@ ugle.
 
 This is a repository for investigating implementations of GNNs for unsupervised clustering.
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = 'William Leeney'
 
 from pkgutil import walk_packages

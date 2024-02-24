213573bb8345bbcb007dc67dfa0c1717dcd48045
willleeney <will.leeney@outlook.com>
2024-02-24 21:27:30 +0000

    working v4

--- ugle/models/cat.py
+++ ugle/models/cat.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 def loss_fn(x, y):
     x = F.normalize(x, dim=-1, p=2)
     y = F.normalize(y, dim=-1, p=2)
-    return 2 - 2 * (x * y).sum(dim=-1)
+    return torch.mean(2 - 2 * (x * y).sum(dim=-1))
 
 
 class EMA:

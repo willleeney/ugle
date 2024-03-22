c41a7a146e58fb8b5bc147821c2a480b33370087
willleeney <will.leeney@outlook.com>
2024-03-22 14:01:15 +0000

    Update run_extra_experiments.sh

--- run_extra_experiments.sh
+++ run_extra_experiments.sh
@@ -5,7 +5,7 @@ for a in sublime vgaer; do
     for d in bat citeseer cora dblp eat texas wisc cornell uat amac amap; do
         lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/complex_networks/default.yaml -da=${d}_${a}_default
         lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/complex_networks/hpo.yaml -da=${d}_${a}
-    end
+    done
 
     for d in cornell wisc texas dblp citeseer cora; do
         lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/33_train_sup.yaml -da=${d}_${a}_default
@@ -18,8 +18,8 @@ for a in sublime vgaer; do
 
         lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/hpo_sup.yaml -da=${d}_${a}
         lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/hpo.yaml -da=${d}_${a}
-    end
+    done
 
     lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/synth.yaml -da=${d}_${a}_default
     lbatch -c 1 -g 1 -m 22 -t 300 -a EMAT022967 -q gpu --conda-env UFR --cmd python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/synth_sup.yaml -da=${d}_${a}_default
-end
+done

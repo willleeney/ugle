4f7ddfeb282a13b654f667f290ba0392d2a7ac78
willleeney <will.leeney@outlook.com>
2024-03-25 10:49:09 +0000

    update bash scripts

--- run_extra_experiments.sh
+++ run_extra_experiments.sh
@@ -2,23 +2,23 @@
 
 
 for d in bat citeseer cora dblp eat texas wisc cornell uat amac amap; do
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/complex_networks/default.yaml -da=${d}_vgaer_default --gpu=0
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/complex_networks/hpo.yaml -da=${d}_vgaer --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/complex_networks/default.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/complex_networks/hpo.yaml -da=${d}_vgaer --gpu=0
 done
 
 for d in cornell wisc texas dblp citeseer cora; do
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/33_train_sup.yaml -da=${d}_vgaer_default --gpu=0
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/33_train.yaml -da=${d}_vgaer_default --gpu=0
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/66_train_sup.yaml -da=${d}_vgaer_default --gpu=0
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/66_train.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/33_train_sup.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/33_train.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/66_train_sup.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/66_train.yaml -da=${d}_vgaer_default --gpu=0
 
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/default_sup.yaml -da=${d}_vgaer_default --gpu=0
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/default.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/default_sup.yaml -da=${d}_vgaer_default --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/default.yaml -da=${d}_vgaer_default --gpu=0
 
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/hpo_sup.yaml -da=${d}_vgaer --gpu=0
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/hpo.yaml -da=${d}_vgaer --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/hpo_sup.yaml -da=${d}_vgaer --gpu=0
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/hpo.yaml -da=${d}_vgaer --gpu=0
 done
 
-python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/synth.yaml -da=${d}_vgaer_default --gpu=0
-python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/synth_sup.yaml -da=${d}_vgaer_default --gpu=0
+python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/synth.yaml -da=${d}_vgaer_default --gpu=0
+python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/synth_sup.yaml -da=${d}_vgaer_default --gpu=0
 
--- run_extra_experiments_1.sh
+++ run_extra_experiments_1.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 for d in bat citeseer cora dblp eat texas wisc cornell uat amac amap; do
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/complex_networks/default.yaml -da=${d}_sublime_default --gpu=1
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/complex_networks/hpo.yaml -da=${d}_sublime --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/complex_networks/default.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/complex_networks/hpo.yaml -da=${d}_sublime --gpu=1
 done
 
 for d in cornell wisc texas dblp citeseer cora; do
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/33_train_sup.yaml -da=${d}_sublime_default --gpu=1
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/33_train.yaml -da=${d}_sublime_default --gpu=1
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/66_train_sup.yaml -da=${d}_sublime_default --gpu=1
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/66_train.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/33_train_sup.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/33_train.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/66_train_sup.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/66_train.yaml -da=${d}_sublime_default --gpu=1
 
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/default_sup.yaml -da=${d}_sublime_default --gpu=1
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/default.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/default_sup.yaml -da=${d}_sublime_default --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/default.yaml -da=${d}_sublime_default --gpu=1
 
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/hpo_sup.yaml -da=${d}_sublime --gpu=1
-    python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/hpo.yaml -da=${d}_sublime --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/hpo_sup.yaml -da=${d}_sublime --gpu=1
+    python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/hpo.yaml -da=${d}_sublime --gpu=1
 done
 
-python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/synth.yaml -da=${d}_sublime_default --gpu=1
-python3 model_evaluations.py -ec==ugle/configs/experiments/paper_configs/unsupervised_limit/synth_sup.yaml -da=${d}_sublime_default --gpu=1
+python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/synth.yaml -da=${d}_sublime_default --gpu=1
+python3 model_evaluations.py -ec=ugle/configs/experiments/paper_configs/unsupervised_limit/synth_sup.yaml -da=${d}_sublime_default --gpu=1
 
